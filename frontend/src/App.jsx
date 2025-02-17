91afe6021548fe79c08fda7f04b7608f0115abca
JohnFrancio <franciaw@gmail.com>
2025-02-17 16:46:05 +0300

    Remove unused imports and clean up URL configuration in the API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,18 @@
 import React from "react";
 import Form from "./components/Form.jsx";
-<<<<<<< HEAD
 import DisplayTables from "./components/DisplayTables.jsx"
+import Header from "./components/Header.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
     return (
         <div>
             <h1>React + Django</h1>
-            <Form />
-            <DisplayTables />
-        </div>
-=======
-import Header from "./components/Header.jsx";
-import Navbar from "./components/Navbar.jsx";
-function App() {
-    return (
-        <>
-            <Header />
-            <div>
-                {/* <Navbar /> */}
-                <main class="flex-1 p-6">                   
+            <main class="flex-1 p-6">                   
                     <Form />
                 </main>
-            </div>
-        </>
->>>>>>> 1eefccec801d29230822dc214e85cd84b0b52b71
+            <DisplayTables />
+        </div>
     );
 }
 
