a1565413d308c66beba96494d04596b391ee615c
ledoa <ledoagael@gmail.com>
2025-02-17 16:30:32 +0300

    Update navbar file, Add Header, styling form.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Form from "./components/Form.jsx";
-
+import Header from "./components/Header.jsx";
+import Navbar from "./components/Navbar.jsx";
 function App() {
     return (
-        <div>
-            <h1>React + Django</h1>
-            <Form />
-        </div>
+        <>
+            <Header />
+            <div>
+                {/* <Navbar /> */}
+                <main class="flex-1 p-6">                   
+                    <Form />
+                </main>
+            </div>
+        </>
     );
 }
 
