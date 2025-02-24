d5cea3e612a09151045a2027df97657c65a8a78a
ledoa <ledoagael@gmail.com>
2025-02-24 11:10:34 +0300

    clean App, Sidebar, Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,3 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Sidebar from "./components/Sidebar.jsx";
-// import Home from "./components/Home.jsx";
-// import Insert from "./components/Insert.jsx";
-// import List from "./components/List.jsx";
-
-// function App() {
-//     return (
-//         <Router>
-//             <div className="flex h-screen">
-//                 <Sidebar />
-//                 <div className="flex-1 lg:pl-64 p-4 overflow-auto">
-//                     <Routes>
-//                         <Route path="/" element={<Home />} />
-//                         <Route path="insert" element={<Insert />} />
-//                         <Route path="list" element={<List />} />
-//                     </Routes>
-//                 </div>
-//             </div>
-//         </Router>
-//     );
-// }
-
-// export default App;
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar.jsx";
 import Home from "./components/Home.jsx";
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,32 +1,3 @@
-// import db from "../assets/db.png";
-
-// const Home = () => {
-
-
-//   return (
-//     <div className="flex flex-col items-center h-screen bg-gradient-to-tr from-[#203139] to-[#438FB2]">
-//       <div className="mt-12">
-//         <h1 className="font-bold text-white text-4xl">Gestion BD</h1>
-//       </div>
-//       <div className="flex flex-col items-center w-150 mt-5">
-//         <h4>
-//           <p className="font-bold text-center text-white">
-//             C&apos;est un application qui permet aux utilisateur de gerer une base ded onnées relationnelle de manière intuitive et sécuriséé. Elle offre une interface utilisateur conviviale pour interagir avec les données sans nécessiter de connaissances avancées en SQL.
-
-
-//             Une application de gestion de base de données est un logiciel permettant de stocker, organiser, manipuler et sécuriser des données de manière structuréé. Elle permet d&apos;effectuer des opérations.
-//           </p>
-//         </h4>
-//       </div>
-//       <div className="flex flex-col items-center">
-//         <img src={db} alt="pic" className="h-85" />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Home;
-
 import db from "../assets/db.png";
 
 const Home = () => {
